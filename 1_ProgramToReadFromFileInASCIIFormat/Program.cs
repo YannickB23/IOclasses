6d57b2a97ed8a218178577ab1be3831e5b874be7
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-02-01 11:49:29 +0100

    ASCII

---
+++ 1_ProgramToReadFromFileInASCIIFormat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ProgramToReadFromFileInASCIIFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\Demo.txt";
+            StreamReader sr;
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+                sr = new StreamReader(fs, Encoding.ASCII, true);
+                int count = 0;
+                while (true)
+                {
+                    int data = fs.ReadByte();
+
+                    //search and count how many times 'q' in the file
+                    if (data == 113)    //113 = decimal value of q
+                    {
+                        //Console.Write("***"); //zorgt voor meer duidelijkheid waar de q staat
+                        count++;
+                    }
+
+                    if (data == -1)
+                    {
+                        break;
+                    }
+                    Console.Write((char)data);  //this works good with ASCII encoding
+                }
+                Console.WriteLine($"\n\nThere are {count} q's in the file.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
